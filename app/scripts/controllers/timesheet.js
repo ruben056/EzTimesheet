8ca9ca17380e761ae35246dfc3cff2ea6ecb7aa5
ruben <demuynck_ruben@hotmail.com>
2015-12-25 14:03:26 +0100

    Added year & month cbo using angular built in directive "ng-options". When another year/month combination is selected the timesheet for that month is shown

--- app/scripts/controllers/timesheet.js
+++ app/scripts/controllers/timesheet.js
@@ -29,6 +29,49 @@ angular.module('eztimesheetApp')
       return daysOfMonth;
     };
 
+    // todo can i use map() function here??
+    var getYearsRange = function(yearsBack, yearsForward, startDate) {
+      var range = moment.range(moment(startDate).subtract(yearsBack, 'years'), moment(startDate).add(yearsForward, 'years'));
+      var result = [];
+      range.by('years', function(momentInRange) {
+        result.push(moment(momentInRange).format('YYYY'));
+      });
+      return result;
+    };
+
+    // todo can i use map() function here??
+    var getMonths = function(startDate) {
+      var range = moment.range(moment(startDate).startOf('year'), moment(startDate).endOf('year'));
+      var result = [];
+      range.by('months', function(momentInRange) {
+        var lbl = moment(momentInRange).format('MMM');
+        var id = moment(momentInRange).format('M');
+        result.push({
+          label: lbl,
+          id: id,
+          idx: id-1
+        });
+      });
+      return result;
+    }
+
     var curDate = new Date();
+    $scope.years = getYearsRange(5, 5, curDate);
+    $scope.selectedYear = moment(curDate).format('YYYY');
+    $scope.yearChanged = function(year) {
+      console.log('actual selected year : ' + year);
+      console.log('$scope.selectedYear : ' + $scope.selectedYear);
+    };
+    $scope.months = getMonths(curDate);
+    $scope.selectedMonth = {
+      id: moment(curDate).format('M'),
+      lbl: moment(curDate).format('MMM'),
+      idx:0
+    };
+    $scope.monthChanged = function(year, month) {
+      var selectedMonth = new Date(year, month.idx);
+      $scope.daysOfMonth = getDaysOfMonth(selectedMonth);  
+    };
+
     $scope.daysOfMonth = getDaysOfMonth(curDate);
   });
